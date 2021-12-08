a80f171e229e909d0cdc344e56e9a96299ba4453
eemeli ruohomäki <eemeliruoh@gmail.com>
2021-12-08 17:16:12 +0200

    alot

--- pages/index.js
+++ pages/index.js
@@ -3,32 +3,33 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="font-inter">
+    <div className="font-sans">
       <Head>
         <title>MEK JS</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Space+Mono&display=swap" rel="stylesheet"></link>
       </Head>
 
       {/*background*/}
-      <div className="z-1 absolute inset-0 h-full w-full bg-gray-200"></div>
+      <div className="z-1 fixed inset-0 min-h-screen h-full w-full bg-hero-pattern"></div>
 
-      <Header className="z-2 relative" />
+      <Header className="z-20 relative" />
 
 
       {/*translation area*/}
-      <div className="z-2 relative py-8">
+      <div className="z-20 relative py-8">
           <form>
-            <textarea rows="7" type="text" placeholder="LaTeX" className="outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 bg-gray-100 p-8 rounded-3xl"/>
-            {/*https://www.w3schools.com/howto/howto_js_copy_clipboard.asp*/}
-            <button onclick="myFunction()" className="flex hover:scale-110 transition duration-100 mx-auto my-8 py-4 px-8 text-2xl text-white bg-green-400 rounded-full shadow-lg">
+            <textarea id="input" rows="7" type="text" placeholder="LaTeX" className="shadow-2xl font-mono outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 max-w-4xl bg-white bg-opacity-75 p-8 rounded-3xl"/>
+            <button className="flex hover:scale-110 transition duration-150 mx-auto my-8 py-4 px-8 text-2xl text-white bg-blue-400 hover:bg-indigo-400 rounded-full shadow-lg">
             Translate
             </button>
           </form>
           <form>
-            <textarea rows="7" type="text" placeholder="LaTeX" className="outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 bg-gray-100 p-8 rounded-3xl"/>
+            <textarea id="output" readonly="readonly" type="text" rows="7" placeholder="CAS Export" className="shadow-2xl font-mono outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 max-w-4xl bg-white bg-opacity-75 p-8 rounded-3xl"/>
             {/*https://www.w3schools.com/howto/howto_js_copy_clipboard.asp*/}
-            <button onclick="myFunction()" className="flex hover:scale-110 transition duration-100 mx-auto my-8 py-4 px-8 text-2xl text-white bg-green-400 rounded-full shadow-lg">
-            Translate
+            <button className="flex hover:scale-110 transition duration-100 mx-auto my-8 py-4 px-8 text-2xl text-white bg-green-400 hover:bg-green-300 rounded-full shadow-lg">
+            Copy
             </button>
           </form>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    boxShadow: {
+      '2xl': '0 0px 50px 0px rgba(0, 0, 0, 0.10)',
+    },
     extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/bg.png')",
+      },
       fontFamily: {
-        sans: ['inter']
-      }
+        sans: ['inter', 'sans'],
+        mono: ['"Space Mono"', 'mono'],
+      },
     },
   },
   variants: {
