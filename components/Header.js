8993611fd514245508bbe1ed39d43b4beb8d2a98
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2021-12-08 11:43:38 +0200

    textarea + header upd

--- components/Header.js
+++ components/Header.js
@@ -6,9 +6,6 @@ function Header() {
                 <span>MEK</span>
                 </a>
             <div className="text-xl font-semibold tracking-tight text-gray-500">
-                <a target="_blank" href="/" className=" hover:text-gray-800 transition duration-200 mr-10">
-                App
-                </a>
                 <a target="_blank" href="https://github.com/matikkaeditorinkaantaja/Matikkaeditorinkaantaja" className="mr-10 hover:text-gray-800 transition duration-100">
                 Github
                 </a>
--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,12 @@ export default function Home() {
       {/*translation area*/}
       <div>
         <div className="">
-        <form>
-          <input type="text" placeholder="LaTeX" className="outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 bg-gray-100 p-8 rounded-3xl"/>
-          <button className="flex hover:scale-110 transition duration-100 mx-auto my-8 py-4 px-8 text-2xl text-white bg-green-400 rounded-full shadow-lg">
-          Translate
-          </button>
-        </form>
+          <form>
+            <textarea rows="7" type="text" placeholder="LaTeX" className="outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 bg-gray-100 p-8 rounded-3xl"/>
+            <button className="flex hover:scale-110 transition duration-100 mx-auto my-8 py-4 px-8 text-2xl text-white bg-green-400 rounded-full shadow-lg">
+            Translate
+            </button>
+          </form>
         </div>
       </div>
     </div>
