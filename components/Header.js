4452d973840f9278e9c4902a88c5c2552aa20b30
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2021-12-08 09:06:58 +0200

    finish header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
 function Header() {
     return (
-        <div className="flex items-center justify-between py-10 px-20">
-                <a href="/" className="text-6xl">
+        <div className="flex items-center justify-between py-4 my-8 mx-16 px-16 bg-white rounded-full shadow-lg">
+                <a href="/" className="flex hover:scale-125 transition duration-100 space-x-3 text-xl font-semibold">
                 <img src="/logo.svg"></img>
+                <span>MEK</span>
                 </a>
             <div className="text-xl font-semibold tracking-tight text-gray-500">
-                <a target="_blank" href="https://github.com/matikkaeditorinkaantaja/Matikkaeditorinkaantaja" className="px-5 hover:text-gray-800 transition duration-100">
+                <a target="_blank" href="/" className=" hover:text-gray-800 transition duration-200 pr-10">
+                App
+                </a>
+                <a target="_blank" href="https://github.com/matikkaeditorinkaantaja/Matikkaeditorinkaantaja" className="pr-10 hover:text-gray-800 transition duration-100">
                 Github
                 </a>
                 <a target="_blank" href="https://math.eem.works" className="hover:text-gray-800 transition duration-100">
