21dc9bd3f67a70514e7fe9ab4889ef3979d85ad8
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2021-12-08 10:58:03 +0200

    add input field and button

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <Header />
+      <Header />
+
+      {/*translation area*/}
+      <div>
+        <div className="">
+        <form>
+          <input type="text" placeholder="LaTeX" className="text-xl flex mx-auto w-3/4 bg-gray-100 p-8 rounded-3xl"/>
+          <button className="flex hover:scale-110 transition duration-100 mx-auto my-8 py-4 px-8 text-2xl text-white bg-green-400 rounded-full shadow-lg">
+          Translate
+          </button>
+        </form>
+        </div>
+      </div>
     </div>
   )
 }
