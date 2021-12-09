9c7b38ac888c901e18fb9a162789fd04f1a6c988
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2021-12-09 13:57:27 +0200

    .,

--- components/Translation.js
+++ components/Translation.js
@@ -4,7 +4,7 @@ function Translation() {
         <div className="z-20 relative py-8">
           <form>
             <textarea spellcheck="false" id="input" rows="7" type="text" placeholder="LaTeX" className="shadow-2xl font-mono outline-none border-0 ring-0 shadow-none text-xl flex mx-auto w-3/4 max-w-4xl bg-white bg-opacity-75 p-8 rounded-3xl"/>
-            <button onClick="interaction(translate)" className="flex hover:scale-110 transition duration-150 mx-auto my-8 py-4 px-8 text-2xl text-white bg-blue-400 hover:bg-indigo-400 rounded-full shadow-lg">
+            <button type="button" onClick="interaction(translate)" className="flex hover:scale-110 transition duration-150 mx-auto my-8 py-4 px-8 text-2xl text-white bg-blue-400 hover:bg-indigo-400 rounded-full shadow-lg">
             Translate
             </button>
           </form>
