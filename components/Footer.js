4adf4cfaba54a6a698fa09c2f72d8816ed9f6ee6
eemeli ruohomäki <eemeliruoh@gmail.com>
2021-12-10 21:35:55 +0200

    fix footer + header

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,26 @@
+import Link from 'next/Link';
+
 function Footer() {
     return (
         <div className="z-2 flex justify-between relative w-3/4 max-w-4xl my-8 mx-auto px-8 py-6 bg-white bg-opacity-75 rounded-3xl">
-            <a href="/" className="items-center space-x-3 text-xl font-semibold">
+            <Link href="/">
+            <a className="items-center space-x-3 text-xl font-semibold">
                 <img src="/logo.svg"></img>
             </a>
+            </Link>
             <div className="text-xl text-right flex flex-col justify-items-end font-semibold tracking-tight text-gray-400">
-            <a target="_blank" href="/" className="hover:text-gray-800 transition duration-200">
+                <Link href="/">
+                <a
+                className="hover:text-gray-800 transition duration-200">
                 App
                 </a>
-                <a target="_blank" href="https://math.eem.works" className="hover:text-gray-800 transition duration-200">
+                </Link>
+                <Link href="/download">
+                <a 
+                className="hover:text-gray-800 transition duration-200">
                 Download
                 </a>
+                </Link>
                 <a target="_blank" href="https://github.com/matikkaeditorinkaantaja/Matikkaeditorinkaantaja" className="transition duration-200 hover:text-gray-800">
                 Github
                 </a>
--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,11 @@ import Link from 'next/Link';
 function Header() {
     return (
         <div className="z-2 relative flex items-center justify-between w-3/4 max-w-4xl my-8 mx-auto px-8 py-6 bg-white bg-opacity-75 rounded-full">
-                <a href="/" className="flex hover:scale-125 transition duration-200 space-x-3 text-xl font-semibold">
+                <Link href="/">
+                <a className="flex hover:scale-125 transition duration-200 space-x-3 text-xl font-semibold">
                 <img src="/logo.svg"></img>
                 </a>
+                </Link>
             <div className="text-xl font-semibold tracking-tight text-gray-400 hidden mobile:block">
                 <Link href="/">
                 <a
