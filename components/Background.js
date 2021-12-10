3f43d37f32a7a67d5da60dabe726460aa04698e7
eemeli ruohomäki <eemeliruoh@gmail.com>
2021-12-10 21:39:14 +0200

    Update Background.js

--- components/Background.js
+++ components/Background.js
@@ -1,7 +1,7 @@
 function bg() {
     return (
         <div>
-            <div className="z-1 fixed inset-0 min-h-screen h-full w-full bg-hero-pattern"></div>
+            <div className="z-1 fixed inset-0 min-h-screen h-full w-full bg-hero-pattern bg-cover"></div>
         </div>
     )
 }
