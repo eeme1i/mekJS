d56118838a09767dfde5a5ad390865134e96aa64
eemeli ruohomäki <56453212+russhh@users.noreply.github.com>
2022-01-26 15:35:10 +0200

    update settings

---
+++ pages/Settings.js
@@ -0,0 +1,58 @@
+function Settings() {
+    return (
+        <div className="mx-auto w-5/6 max-w-4xl p-8 bg-gray-200 rounded-3xl tracking-tighter font-medium text-gray-700">
+            <p
+            className="text-5xl">
+                Settings
+            </p>
+            <div className="my-8">
+                <p className="text-3xl pb-4">Tila</p>
+                <div className="flex justify-center">
+                    <button
+                    type="button"
+                    className="text-2xl tracking-tighter font-medium w-3/4 py-4 bg-white rounded-2xl hover:bg-green-400 transition duration-200">
+                       Math 1 
+                    </button>
+                    <button
+                    type="button"
+                    className="text-2xl tracking-tighter font-medium w-3/4 py-4 hover:bg-green-400 bg-white rounded-2xl mx-8 transition duration-200">
+                       Math 2 
+                    </button>
+                    <button
+                    type="button"
+                    className="text-2xl tracking-tighter font-medium w-3/4 py-4 hover:bg-green-400 bg-white rounded-2xl transition duration-200">
+                       Physics
+                    </button>
+                </div>
+            </div>
+            <div className="my-8">
+                <p className="text-3xl pb-4">Tila</p>
+                <div className="flex justify-center">
+                    <button
+                    type="button"
+                    className="text-2xl tracking-tighter font-medium w-3/4 py-4 bg-white rounded-2xl hover:bg-green-400 mx-4 transition duration-200">
+                      Vaalea 
+                    </button>
+                    <button
+                    type="button"
+                    className="text-2xl tracking-tighter font-medium w-3/4 py-4 hover:bg-green-400 bg-white rounded-2xl mx-4 transition duration-200">
+                       Tumma 
+                    </button>
+                </div>
+            </div>
+            <div className="my-8">
+                <p className="text-3xl pb-4">Muu</p>
+                <div className="text-2xl">
+                    <div className="flex">
+                        <p className=" bg-white p-4">Geometriatila</p>
+                    </div>
+                    <div className="">
+                        <p>Geometriatila</p>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Settings
